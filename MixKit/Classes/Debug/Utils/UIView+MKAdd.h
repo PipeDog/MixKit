b718d98c5d31c99610e4e8adc3d0c004803d1260
liang <leiliang4@xdf.cn>
2021-07-06 11:35:56 +0800

    remove space line

--- MixKit/Classes/Debug/Utils/UIView+MKAdd.h
+++ MixKit/Classes/Debug/Utils/UIView+MKAdd.h
@@ -12,23 +12,14 @@ NS_ASSUME_NONNULL_BEGIN
 @interface UIView (MKAdd)
 
 @property (nonatomic, assign) CGFloat top;
-
 @property (nonatomic, assign) CGFloat bottom;
-
 @property (nonatomic, assign) CGFloat left;
-
 @property (nonatomic, assign) CGFloat right;
-
 @property (nonatomic, assign) CGFloat width;
-
 @property (nonatomic, assign) CGFloat height;
-
 @property (nonatomic, assign) CGFloat centerX;
-
 @property (nonatomic, assign) CGFloat centerY;
-
 @property (nonatomic, assign) CGSize size;
-
 @property (nonatomic, assign) CGPoint origin;
 
 @end
